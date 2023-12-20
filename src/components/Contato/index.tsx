f9c54a437465415072e02ca1dc144566807085a6
Aldo Santos <aldoacesso@gmail.com>
2023-12-20 15:58:02 -0300

    Criado o estado de troca de botões

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Contato = () => (
-  <S.Card>
-    <S.Titulo>Nome do Contato</S.Titulo>
-    <S.Descricao type="email" placeholder="Digite o E-mail" />
-    <S.Descricao type="phone" placeholder="Digite o Telefone" />
-    <S.Complemento placeholder="Digite um complemento" />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+type Props = {
+  titulo: string
+  descricao1: string
+  descricao2: string
+  complemento: string
+}
+
+const Contato = ({ titulo, descricao1, descricao2, complemento }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Descricao value={descricao1} type="email" placeholder="E-mail" />
+      <S.Descricao value={descricao2} type="phone" placeholder="Telefone" />
+      <S.Complemento value={complemento} placeholder="Digite um complemento" />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.Botao>Salvar</S.Botao>
+            <S.Botao onClick={() => setEstaEditando(false)}>Cancelar</S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.Botao>Remover</S.Botao>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 
 export default Contato
--- src/containers/ListaDeContatos/index.tsx
+++ src/containers/ListaDeContatos/index.tsx
@@ -1,22 +1,41 @@
 import Contato from '../../components/Contato'
 import { Container } from './styles'
 
+const contatos = [
+  {
+    titulo: 'André',
+    descricao1: 'email@mail.com',
+    descricao2: '81 678678678',
+    complemento: 'Tudo certo mô vei'
+  },
+  {
+    titulo: 'Rafael',
+    descricao1: '',
+    descricao2: '',
+    complemento: ''
+  },
+  {
+    titulo: 'Marta',
+    descricao1: 'email@mail.com',
+    descricao2: '81 678678678',
+    complemento: 'Tudo certo'
+  }
+]
+
 const ListaDeContatos = () => (
   <Container>
     <p>2 contatos: `&quot;`termo`&ldquo;`</p>
     <ul>
-      <li>
-        <Contato />
-      </li>
-      <li>
-        <Contato />
-      </li>
-      <li>
-        <Contato />
-      </li>
-      <li>
-        <Contato />
-      </li>
+      {contatos.map((c) => (
+        <li key={c.titulo}>
+          <Contato
+            titulo={c.titulo}
+            descricao1={c.descricao1}
+            descricao2={c.descricao2}
+            complemento={c.complemento}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
